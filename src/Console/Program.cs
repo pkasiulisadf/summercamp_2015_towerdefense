f203d63045a39a12a4074556a7251d4332db45b7
liudasstrimaitis <liudas.strimaitis@gmail.com>
2015-08-15 13:03:12 +0300

    SignalR hub

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.Cors;
 
-namespace Console
+namespace SignalRSelfHost
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Hello world!");
-            System.Console.ReadLine();
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            string url = "http://localhost:8080";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void HelloWorld()
+        {
         }
     }
 }
