2547c34d3c00a35f1333d565eaa107601eeb6162
karolisz <karolis2xg@gmail.com>
2015-08-15 18:46:32 +0300

    Map object(cells)

---
+++ src/Console/Objects/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Adform.SummerCamp.TowerDefense.Console.Objects
+{
+    class Cell
+    {
+        private Guid CellId { set; get; }
+        private uint Posx { set; get; }
+        private uint PosY { set; get; }
+
+        public Cell(uint PosX, uint PosY)
+        {
+            this.Posx = Posx;
+            this.PosY = PosY;
+            this.CellId = Guid.NewGuid();
+        }
+    }
+}
---
+++ src/Console/Objects/Map.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Adform.SummerCamp.TowerDefense.Console.Objects
+{
+    class Map
+    {
+        List<Cell> Cells = new List<Cell>();
+        List<int> intai = new List<int>();
+    }
+}
