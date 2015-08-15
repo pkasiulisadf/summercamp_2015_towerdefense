6f212e46364e40c391b1c9f95a63f3266d5b8f97
karolisz <karolis2xg@gmail.com>
2015-08-15 18:49:25 +0300

    Added type field

--- src/Console/Objects/Cell.cs
+++ src/Console/Objects/Cell.cs
@@ -7,11 +7,13 @@ namespace Adform.SummerCamp.TowerDefense.Console.Objects
         private Guid CellId { set; get; }
         private uint Posx { set; get; }
         private uint PosY { set; get; }
+        private string Type { set; get; }
 
-        public Cell(uint PosX, uint PosY)
+        public Cell(uint PosX, uint PosY, string Type)
         {
             this.Posx = Posx;
             this.PosY = PosY;
+            this.Type = Type;
             this.CellId = Guid.NewGuid();
         }
     }
